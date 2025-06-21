cfade0594ad8abc37a9856f0d938bf7b965ebec4
Fernando Salom <fer@rudo.es>
2025-06-21 22:05:42 +0200

    Add initial scaffolding project

---
+++ microworkout/driven/db/users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class UserDBO(AbstractUser):
+    """Implement your own model"""
+    pass
---
+++ microworkout/driving/api/v1/urls.py
@@ -0,0 +1,5 @@
+from django.urls import include, path
+
+urlpatterns = [
+    # path('users/', include('driving.api.v1.users.urls')),
+]
