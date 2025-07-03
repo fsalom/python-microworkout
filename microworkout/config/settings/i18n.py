224c59bfebb91d2a1bd0d2e5ed9adeabc4e5c45a
Fernando Salom <fer@rudo.es>
2025-07-03 22:25:33 +0200

    Add settings

---
+++ microworkout/config/settings/i18n.py
@@ -0,0 +1,15 @@
+from .tasks import *
+import importlib.resources as pkg_resources
+
+MIDDLEWARE.append('django.middleware.locale.LocaleMiddleware')
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+    str(pkg_resources.files("commons_package").joinpath("locale")),
+]
+USE_I18N = True
+USE_L10N = True
+ugettext = lambda s: s
+LANGUAGES = (
+    ('es', ugettext('Spanish')),
+    ('en', ugettext('English')),
+)
